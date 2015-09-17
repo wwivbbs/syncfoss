7ebb3b8c4221916ffb4d3f1e2d96c1ee1173fa8e
RushFan <rushfan@wwiv.us>
2015-09-16 20:05:36 -0700

    got dosxtrn compiling with 16-bit DDKs (English) This is WINDDK version 2600

--- dosxtrn/make.bat
+++ dosxtrn/make.bat
@@ -1,2 +1,3 @@
-@echo off
-cl /I\msvc15\include /I.. /I\ntddk\inc /I..\..\xpdev /AS /Fc /Fm /Gs dosxtrn.c 
+rem @echo off
+set LIB=C:\WINDDK\2600\lib\win_me\
+C:\WINDDK\2600\bin\win_me\bin16\cl.exe /IC:\MSVC\INCLUDE /IC:\WINDDK\2600\inc\win_me\inc16 /I.. /IC:\WINDDK\2600\inc\ddk  /I..\..\xpdev /IC:\WINDDK\2600\inc\ddk\wxp /AS /Fc /Fm /Gs dosxtrn.c libw.lib slibce.lib
