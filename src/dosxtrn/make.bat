926bd8df6403ff651fc1cb12aa80708bd1cde540
RushFan <rushfan@wwiv.us>
2015-09-19 20:18:12 -0700

    created nmake makefile for dosxtrn.exe

--- src/dosxtrn/make.bat
+++ src/dosxtrn/make.bat
@@ -1,3 +1,3 @@
 rem @echo off
 set LIB=C:\WINDDK\2600\lib\win_me\
-C:\WINDDK\2600\bin\win_me\bin16\cl.exe /IC:\MSVC\INCLUDE /IC:\WINDDK\2600\inc\win_me\inc16 /I.. /IC:\WINDDK\2600\inc\ddk  /I..\..\xpdev /IC:\WINDDK\2600\inc\ddk\wxp /AS /Fc /Fm /Gs dosxtrn.c libw.lib slibce.lib
+C:\WINDDK\2600\bin\win_me\bin16\cl.exe /IC:\MSVC\INCLUDE /IC:\WINDDK\2600\inc\win_me\inc16 /I..\sbbsexec /IC:\WINDDK\2600\inc\ddk  /I..\xpdev /IC:\WINDDK\2600\inc\ddk\wxp /AS /Fc /Fm /Gs dosxtrn.c libw.lib slibce.lib
